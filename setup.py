1ba00a6db021876149bd78dca93d3270bc541f7f
hohav <hohav@fastmail.com>
2018-06-29 11:07:40 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author="melkor",
+    author_email="hohav@fastmail.com",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    description="Parsing library for SSBM replay files",
+    install_requires=['py-ubjson'],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    name="py-slippi",
+    packages=setuptools.find_packages(),
+    python_requires='~=3.6',
+    url="https://github.com/hohav/py-slippi",
+    version="1.0.0",
+)

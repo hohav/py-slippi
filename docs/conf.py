b4daad8d85366fd0f79ae6ebd83608a9a79e3864
hohav <hohav@fastmail.com>
2018-07-14 13:58:59 -0700

    bump version to 1.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,8 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'py-slippi'
 author = 'melkor'
 
-version = '1.0.2'
-release = '1.0.2'
+version = '1.0.3'
+release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------

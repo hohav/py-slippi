a3eaa2dd1e9ba35aa96b9b4592bee72a943f12d1
Nikhil Narayana <nikhil.narayana@live.com>
2020-03-29 09:26:37 -0400

    fix formatting and variable names

--- slippi/metadata.py
+++ slippi/metadata.py
@@ -8,13 +8,13 @@ from slippi.util import *
 
 class Metadata(Base):
     """Miscellaneous data relevant to the game but not directly provided by Melee."""
-    def __init__(self, date, duration, platform, players, console_nick, json):
+    def __init__(self, date, duration, platform, players, console_name, _json):
         self.date = date #: :py:class:`datetime`: Game start date & time
         self.duration = duration #: :py:class:`int`: Duration of game, in frames
         self.platform = platform #: :py:class:`Platform`: Platform the game was played on (console/dolphin)
         self.players = players #: tuple(optional(:py:class:`Player`)): Player metadata by port (port 1 is at index 0; empty ports will contain None)
-        self.console_nick = console_nick #: :py:class:`str`: Name of the console the game was played on
-        self.json = json #: :py:class `dict`: Copy of json metadata for forwards compatibility with replays
+        self.console_name = console_name #: optional(:py:class:`str`): Name of the console the game was played on, None if not present
+        self._json = _json #: :py:class:`dict`: Copy of json metadata for forwards compatibility with replays
 
     @classmethod
     def _parse(cls, json):
@@ -25,40 +25,38 @@ class Metadata(Base):
         try: duration = 1 + json['lastFrame'] - evt.FIRST_FRAME_INDEX
         except KeyError: duration = None
         platform = cls.Platform(json['playedOn'])
-        console_nick = None
-        try: console_nick = json['consoleNick']
-        except KeyError: pass
+        try: console_name = json['consoleNick']
+        except KeyError: console_name = None
         players = [None, None, None, None]
         for i in PORTS:
             try: players[i] = cls.Player._parse(json['players'][str(i)])
             except KeyError: pass
-        return cls(date=date, duration=duration, platform=platform, players=tuple(players), console_nick=console_nick, json=json)
+        return cls(date=date, duration=duration, platform=platform, players=tuple(players), console_name=console_name, _json=json)
 
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             return NotImplemented
-        return self.date == other.date and self.duration == other.duration and self.platform == other.platform and self.players == other.players
+        return self.date == other.date and self.duration == other.duration and self.platform == other.platform and self.players == other.players and self.console_name == other.console_name
 
 
     class Player(Base):
-        def __init__(self, characters, name):
+        def __init__(self, characters, netplay_name):
             self.characters = characters #: dict(:py:class:`slippi.id.InGameCharacter`, :py:class:`int`): Character(s) used, with usage duration in frames (for Zelda/Sheik)
-            self.name = name #: :py:class `str`: Netplay name of player if played on dolphin, otherwise empty string 
+            self.netplay_name = netplay_name #: optional(:py:class:`str`): Netplay name of player if played on dolphin, None if not present
 
         @classmethod
         def _parse(cls, json):
             characters = {}
-            name = ""
             for char_id, duration in json['characters'].items():
                 characters[id.InGameCharacter(int(char_id))] = duration
-            try: name = json['names']['netplay']
-            except KeyError: pass
-            return cls(characters, name)
+            try: netplay_name = json['names']['netplay']
+            except KeyError: netplay_name = None
+            return cls(characters, netplay_name)
 
         def __eq__(self, other):
             if not isinstance(other, self.__class__):
                 return NotImplemented
-            return self.characters == other.characters
+            return self.characters == other.characters and self.netplay_name == other.netplay_name
 
 
     class Platform(Enum):
